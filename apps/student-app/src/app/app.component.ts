b2d773f62093adf63eae60c0ec33fefc722aa81f
darkyzhou <darkyoooooo@gmail.com>
2021-02-04 15:14:24 +0800

    build: improve css splitting
    GitOrigin-RevId: 0d1cf180440698f16f49ad4265c7c0a72113d557

--- apps/student-app/src/app/app.component.ts
+++ apps/student-app/src/app/app.component.ts
@@ -2,13 +2,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { UntilDestroy } from '@ngneat/until-destroy';
 import { PrimeNGConfig } from 'primeng/api';
 
+@UntilDestroy()
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-@UntilDestroy()
 export class AppComponent {
   constructor(private readonly primengConfig: PrimeNGConfig) {
     this.primengConfig.ripple = true;
