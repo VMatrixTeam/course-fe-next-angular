02115f7eec29cc8308df9a30498860bdef41be74
darkyzhou <darkyoooooo@gmail.com>
2021-04-12 11:06:32 +0800

    chore(ng-tailwind-jit.js): improve log texts
    GitOrigin-RevId: abcec1e46953be2affa683058d576d349804c117

--- scripts/ng-tailwindcss-jit.js
+++ scripts/ng-tailwindcss-jit.js
@@ -1,14 +1,12 @@
-const fs = require("fs");
-console.log('Running postinstall script')
+const fs = require('fs');
+console.log('Patching Angular webpack for TailwindCSS JIT');
 
-const pathToStylesFile =
-  "node_modules/@angular-devkit/build-angular/src/webpack/configs/styles.js";
+const pathToStylesFile = 'node_modules/@angular-devkit/build-angular/src/webpack/configs/styles.js';
 
 try {
-  let data = fs.readFileSync(pathToStylesFile, "utf-8");
-
+  let data = fs.readFileSync(pathToStylesFile, 'utf-8');
   data = data.replace(/'tailwindcss'/g, "'@tailwindcss/jit'");
   fs.writeFileSync(pathToStylesFile, data);
-} catch {
-  console.log("Post-install error");
+} catch (e) {
+  console.error(e);
 }
